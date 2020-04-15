3da809def0a9944de463f4448f73abd09ed7f4c1
George <george.strike@outlook.com>
2020-04-15 20:31:21 +0100

    Removed default files, show recipe changes

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -35,13 +35,7 @@ export class NavMenu extends Component {
                                     <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                                </NavItem>
-                                <NavItem>
                                     <NavLink tag={Link} className="text-dark" to="/recipe">Recipe</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                                 </NavItem>
                                 <LoginMenu>
                                 </LoginMenu>
--- ClientApp/src/components/Recipes/RecipeView.js
+++ ClientApp/src/components/Recipes/RecipeView.js
@@ -4,20 +4,59 @@ import { Row, Col, Table, Tag } from 'antd';
 import './RecipeView.scss';
 
 const { Column } = Table;
+
 const dietryColourList = {
     Vegan: "#06BA4A",
     Vegetarian: "green",
     Nut: "#78281F",
     Fish: "blue"
 }
+const coreMacrosList = {
+    Calories: "energy",
+    Fat: "fat",
+    Carbohydrate: "carbs",
+    Protein: "protein"
+}
+
 export class RecipeView extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             hideRecipes: this.props.hideRecipes,
-            recipe: this.props.recipeData
+            recipe: this.props.recipeData,
+            servicesTotal: 0
         };
+    }
+
+    setTagColour(recipe) {
+        let recipeTag = recipe.toLowerCase();
+        let recipeTagColour = recipeTag.includes("vegan") ? dietryColourList.Vegan
+            : recipeTag.includes("vegetarian") ? dietryColourList.Vegetarian
+            : recipeTag.includes("nut") ? dietryColourList.Nut
+            : recipeTag.includes("fish") ? dietryColourList.Fish
+            : "geekblue"
+
+        return recipeTagColour;
+    }
+
+    setNutrientValues(nutrient) {
+        let nutrientsArr = [];
+        Object.keys(nutrient).forEach(item => {
+            if (nutrient[item]) {
+                let currentNutrientName = nutrient[item].label.toLowerCase();
+                if (currentNutrientName === coreMacrosList.Calories || currentNutrientName === coreMacrosList.Carbohydrate || currentNutrientName === coreMacrosList.Fat || currentNutrientName === coreMacrosList.Protein) {
+                    nutrientsArr.push(nutrient[item]);
+                }
+            }
+        })
+        return nutrientsArr;
+    }
+
+    //calculateMacrosPerServing = (macro) => }
+
+    componentDidMount() {
+
     }
 
     renderRecipeView(recipe) {
@@ -25,7 +64,7 @@ export class RecipeView extends Component {
             <div className="recipe-data">
                 <Row>
                     <Col sm={24}>
-                        <Table dataSource={recipe.map(element => { return (element)})} rowKey="url">
+                        <Table dataSource={recipe.map(element => { return (element) })} rowKey="url" pagination={{ pageSize: 6 }}>
                             <Column title="Image" dataIndex="image" key="image" render={
                                 image =>
                                     (<img className="recipe-img" alt={image} src={image} />)}
@@ -38,17 +77,27 @@ export class RecipeView extends Component {
                                     {item.map((element, index) => {
                                         return (
                                             <li key={index} className="dietry-element">
-                                                <Tag color={element.toLowerCase().includes("vegan") ? dietryColourList.Vegan
-                                                    : element.toLowerCase().includes("vegetarian") ? dietryColourList.Vegetarian
-                                                        : element.toLowerCase().includes("nut") ? dietryColourList.Nut
-                                                            : element.toLowerCase().includes("fish") ? dietryColourList.Fish
-                                                                : "geekblue"} key="element">{element}</Tag>
+                                                <Tag color={this.setTagColour(element)} key="element">{element}</Tag>
                                             </li>
                                         );
                                     })}
                                 </ul>
                             )} />
-
+                            <Column title="Marcos (per serving)" dataIndex="totalNutrients" key="totalNutrients" render={nutrient => (
+                                <div>
+                                    {Array.from([nutrient]).map((n, index) => {
+                                        return (
+                                            <ul className="list-of-nutrients">
+                                                {
+                                                    this.setNutrientValues(n).map(element => {
+                                                        return (<li key={index++} className="nutrient-element"><p className="marcoText">{element.label + ": " + element.quantity + element.unit}</p></li>);
+                                                    })
+                                                }
+                                            </ul>                                       
+                                        );
+                                    })}
+                                </div>
+                            )} />
                         </Table>
                     </Col>
                 </Row>
